8ebff711a4fa0838c256fd6d146f5fb8fa6ef880
kholikrr <117918676+kholikrr@users.noreply.github.com>
2023-01-26 20:42:05 +0700

    cek login
    mengecek login sebagai admin atau user

---
+++ cek_login.php
@@ -0,0 +1,49 @@
+<?php
+// mengaktifkan session pada php
+session_start();
+
+// menghubungkan php dengan koneksi database
+include 'config.php';
+
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($koneksi,"SELECT * FROM users WHERE username='$username' and password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+
+ $data = mysqli_fetch_assoc($login);
+
+ // cek jika user login sebagai admin
+ if($data['level']=="admin"){
+
+  // buat session login dan username
+  $_SESSION['username'] = $username;
+  $_SESSION['level'] = "admin";
+  // alihkan ke halaman dashboard admin
+  header("location:home.php");
+
+ // cek jika user login sebagai pengguna
+ }else if($data['level']=="users"){
+  // buat session login dan username
+  $_SESSION['username'] = $username;
+  $_SESSION['level'] = "users";
+  // alihkan ke halaman dashboard pengguna
+  header("location:home.php");
+
+ }else{
+
+  // alihkan ke halaman login kembali
+  header("location:index.php?pesan=gagal");
+ } 
+}else{
+ header("location:index.php?pesan=gagal");
+}
+
+?>
